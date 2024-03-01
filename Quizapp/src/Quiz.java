e65b1cf20bea09bfde4bcd4586ea795c98f093d6
Akhil Mohan R <115225714+AkhilMohan-R@users.noreply.github.com>
2024-03-01 21:25:00 +0530

    Initial Commit.

---
+++ Quizapp/src/Quiz.java
@@ -0,0 +1,103 @@
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class Quiz implements ActionListener{
+    JFrame jf;
+    JLabel j1;
+    JLabel background;
+    JLabel name;
+    JLabel background1;
+    JTextField n;
+    JButton rules;
+    JButton back;
+	
+	public Quiz() {
+		jf = new JFrame("QUIZ");
+		jf.setLayout(null);
+		
+		
+		jf.getContentPane().setBackground(Color.WHITE);
+		jf.setSize(1403,739);
+		jf.setLocation(270, 160);
+		
+		
+		j1 = new JLabel("Welcome to Psc Inspire");
+	    j1.setBounds(870, 80, 550, 50);
+	    j1.setFont(new Font("Times New Roman",Font.BOLD | Font.ITALIC,44));
+	    j1.setForeground(new Color(255,250,240));
+	    j1.setOpaque(false);
+	    
+	    j1.setHorizontalAlignment(SwingConstants.CENTER);
+	    jf.add(j1);
+	    
+	    name = new JLabel("Enter your name");
+	    name.setBounds(870, 350, 550, 50);
+	    name.setFont(new Font("Times New Roman",Font.BOLD ,30));
+	    name.setForeground(new Color(25, 25, 112));
+	    name.setOpaque(false);
+	    
+	    name.setHorizontalAlignment(SwingConstants.CENTER);
+	    jf.add(name);
+	   
+		JLabel background = new JLabel(new ImageIcon("C:\\Users\\akhil\\eclipse-workspace\\Quizapp\\src\\icons\\quiz.jpg"));
+        background.setBounds(0, 0,900, 700);
+        jf.add(background);
+        
+        JLabel background1 = new JLabel(new ImageIcon("C:\\Users\\akhil\\eclipse-workspace\\Quizapp\\src\\icons\\line.jpg"));
+        background1.setBounds(890, 0,500, 195);
+        jf.add(background1);
+        
+        n = new JTextField();
+        n.setBounds(1000, 420, 300, 28);
+        n.setFont(new Font("Times New Roman",Font.CENTER_BASELINE,18));
+        jf.add(n);
+        
+        rules = new JButton("Rules");
+        rules.setBounds(1000, 500, 100, 30);
+        rules.addActionListener(this);
+        rules.setFont(new Font("Times New Roman",Font.BOLD,20));
+        rules.setBackground(new Color(25, 25, 112));
+        rules.setForeground(Color.WHITE);
+        jf.add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(1195, 500, 100, 30);
+        back.addActionListener(this);
+        back.setFont(new Font("Times New Roman",Font.BOLD,20));
+        back.setBackground(new Color(25, 25, 112));
+        back.setForeground(Color.WHITE);
+        jf.add(back);
+        
+        
+        jf.setVisible(true);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String a[]) {
+		new Quiz();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == rules) {
+			String name = n.getText();
+			new Rules(name);
+			jf.setVisible(false);
+		
+		}else if(e.getSource() == back) {
+			jf.setVisible(false);
+		}
+		
+	}
+
+}
---
+++ Quizapp/src/Rules.java
@@ -0,0 +1,75 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class Rules extends JFrame implements ActionListener{
+	
+	String name;
+	JButton start,back;
+	public Rules(String name) {
+		this.name=name;
+		getContentPane().setBackground(Color.WHITE);
+		setLayout(null);
+		
+		JLabel j1 = new JLabel("Hi, " +name+ ".  Welcome to Psc Inspire!");
+	    j1.setBounds(70, 20, 500, 50);
+	    j1.setFont(new Font("Times New Roman",Font.BOLD | Font.ITALIC,25));
+	    j1.setForeground(new Color(0, 0, 128));
+	    j1.setOpaque(false);
+	    j1.setHorizontalAlignment(SwingConstants.CENTER);
+	    add(j1);
+	    
+	    JLabel rules = new JLabel();
+	    rules.setBounds(50, 80, 500, 350);
+	    rules.setFont(new Font("Times New Roman",Font.PLAIN | Font.ITALIC,18));
+        rules.setText(
+        		"<html>"+
+                "1. The Quiz contains 20 Multiple Choice Questions." + "<br><br>" +
+        		"2. Read the questions carefully and then answer them." + "<br><br>" +
+         		"3. Each questions will be given points. There is no negative marking." + "<br><br>" +
+                "4. Make sure you are connected to a stable Wi-fi or cable internet." + "<br><br>" +
+        		"5. You are free to take the quiz several times until you get 100% result." + "<br><br>" +
+                "<html>"
+        );
+	    rules.setOpaque(false);
+	    rules.setHorizontalAlignment(SwingConstants.CENTER);
+	    add(rules);
+	    
+	    start = new JButton("Start");
+	    start.setBounds(360, 410, 100, 30);
+	    start.addActionListener(this);
+	    start.setFont(new Font("Times New Roman",Font.BOLD,20));
+	    start.setBackground(new Color(25, 25, 112));
+	    start.setForeground(Color.WHITE);
+        add(start);
+        
+        back = new JButton("Back");
+        back.setBounds(140, 410, 100, 30);
+        back.addActionListener(this);
+        back.setFont(new Font("Times New Roman",Font.BOLD,20));
+        back.setBackground(new Color(25, 25, 112));
+        back.setForeground(Color.WHITE);
+        add(back);
+		
+		setSize(650,540);
+		setLocation(570,260);
+		setVisible(true);
+	}
+	public static void main(String a[]) {
+		new Rules("User");
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == start) {
+			
+		}else {
+			setVisible(false);
+			new Quiz();
+		}
+		
+	}
+
+}
